39d744da298d730ec7477aca10bc5cbce374be1e
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-09-01 12:45:03 -0400

    :white_check_mark: add promoEventPageView coverage + refactor undefined window id test to bottom

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,23 @@
-import { generateEventIdClientSide } from '../src/eventId';
+import { 
+  promoEventPageView,
+  promoEventAddPaymentInfo,
+  promoEventAddToCart,
+  promoEventCompleteRegistration,
+  promoEventDonate,
+  promoEventInitiateCheckout,
+  promoEventLead,
+  promoEventPurchase,
+  promoEventSearch,
+  promoEventStartTrial,
+  promoEventSubmitApplication,
+  promoEventSubscribe,
+  promoEventViewContent,
+} from '../src/index';
+
+import {
+  generateEventIdClientSide,
+} from '../src/eventId';
+
 beforeAll(() => {
   /* @ts-ignore */
   global.window = jest.fn();
@@ -15,6 +34,7 @@ beforeAll(() => {
       },
     },
   };
+  window['dataLayer'] = [];
 });
 
 describe('generateEventIdClientSide', () => {
@@ -22,6 +42,52 @@ describe('generateEventIdClientSide', () => {
     expect(generateEventIdClientSide()).toBe('testStart.testUniqueEventId');
   });
 });
+describe('gtmEventPageView', () => {
+  it('executes', () => {
+    expect(promoEventPageView({})).toBe(undefined);
+  });
+});
+describe(  'PromoEventPageView', () => {
+
+});
+describe('PromoEventAddPaymentInfo', () => {
+
+});
+describe('PromoEventAddToCart', () => {
+
+});
+describe('PromoEventCompleteRegistration', () => {
+
+});
+describe('PromoEventDonate', () => {
+
+});
+describe('PromoEventInitiateCheckout', () => {
+
+});
+describe('PromoEventLead', () => {
+
+});
+describe('PromoEventPurchase', () => {
+
+});
+describe('PromoEventSearch', () => {
+
+});
+describe('PromoEventStartTrial', () => {
+
+});
+describe('PromoEventSubmitApplication', () => {
+
+});
+describe('PromoEventSubscribe', () => {
+
+});
+describe('PromoEventViewContent', () => {
+
+});
+/* This needs to be last in the file due to its mangling of the global.window 
+ * property. */
 describe('generateEventIdClientSide', () => {
   it('returns undefined when google_tag_manager is not present on window', () => {
     /* @ts-ignore */
@@ -30,3 +96,5 @@ describe('generateEventIdClientSide', () => {
     expect(generateEventIdClientSide()).toBe(undefined);
   });
 });
+
+
